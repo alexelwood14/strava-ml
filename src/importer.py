61cfc9462ec3a91dd07ef374ce0057b3ba0eb69c
alexelwood14 <alex.elwood14@outlook.com>
2022-03-07 20:27:22 +0000

    Reformatted data

--- src/importer.py
+++ src/importer.py
@@ -1,17 +1,22 @@
 import csv
+import numpy as np
 
 
 def clean_data(data):
     # Convert to correct data types
     for row in range(1, len(data)):
         for atr in range(len(data[row])):
+            # Try to replace string with int
             try:
                 data[row][atr] = int(data[row][atr])
             except ValueError:
+                # Try to replace string with float
                 try:
                     data[row][atr] = float(data[row][atr])
                 except ValueError:
-                    continue
+                    # Change missing data to None primitive
+                    if data[row][atr] == '':
+                        data[row][atr] = None
 
 
 def import_data():
@@ -19,14 +24,15 @@ def import_data():
     data = []
     with open(filename) as csv_file:
         raw_data = csv.reader(csv_file, delimiter=',')
-
+        headers = next(raw_data)
         for row in raw_data:
             data.append(row)
 
-    return data
+    return headers, np.array(data).T
 
 
 if __name__ == '__main__':
-    data = import_data()
+    headers, data = import_data()
     clean_data(data)
     print(data)
+    print(headers)
