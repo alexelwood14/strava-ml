15d6e8e919d4c24acd278a62913146520b40e587
alexelwood14 <alex.elwood14@outlook.com>
2022-03-08 15:54:26 +0000

    Simple viewer created

--- src/importer.py
+++ src/importer.py
@@ -14,33 +14,50 @@ def clean_data(data):
                 try:
                     data[row][atr] = float(data[row][atr])
                 except ValueError:
-                    # Change missing data to None primitive
-                    if data[row][atr] == '':
-                        data[row][atr] = None
+                    continue
 
 
-def import_data():
-    filename = 'activities.csv'
-    data = []
-    with open(filename) as csv_file:
-        raw_data = csv.reader(csv_file, delimiter=',')
-        headers = next(raw_data)
-        for row in raw_data:
-            data.append(row)
+class Data:
+    def __init__(self):
+        self.file_name = 'activities.csv'
+        self.__data = []
+        with open(self.file_name) as csv_file:
+            raw_data = csv.reader(csv_file, delimiter=',')
+            self.headers = next(raw_data)
+            for row in raw_data:
+                self.__data.append(row)
 
-    return headers, np.array(data).T
+        self.__data = np.array(self.__data).T
 
+        clean_data(self.__data)
 
-def format_data(headers, d):
-    data = {}
-    for i in range(len(headers)):
-        data[headers[i]] = d[i]
+        # Reformat the data
+        data = {}
+        for i in range(len(self.headers)):
+            data[self.headers[i]] = self.__data[i]
 
-    return data
+        self.__data = data
+
+    def get_data(self, headers, omit_nones=True):
+        # Create array of flags to be included
+        include = [True for i in range(len(self.__data['Activity ID']))]
+        filtered_data = {}
+
+        if omit_nones:
+            for header in headers:
+                for atr in range(len(self.__data[header])):
+                    if self.__data[header][atr] == '':
+                        include[atr] = False
+
+        for header in headers:
+            filtered_data[header] = []
+            for atr in range(len(self.__data[header])):
+                if include[atr]:
+                    filtered_data[header].append(self.__data[header][atr])
+
+        return filtered_data
 
 
 if __name__ == '__main__':
-    headers, data = import_data()
-    clean_data(data)
-    print(data)
-    print(headers)
+    data_set = Data()
+    data = data_set.get_data(['Activity ID', 'Perceived Exertion'])
